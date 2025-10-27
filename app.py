860bd38038f7377f73a2628ada0d34c99a8a7942
Ubuntu <UsuarioSV@MV-006.kbxjhwgr2gruzimispp1x2wiqa.cx.internal.cloudapp.net>
2025-10-27 23:09:50 +0000

    Paso primero

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def hello():
-    return "Hola Mundo app 2", 200
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hola desde app B', 200
+
+@app.route('/health', methods=['GET'])
+def call_health():
+    return 'OK', 200
+
+@app.route('/startup', methods=['GET'])
+def call_startup():
+    return 'OK', 200
+    
+@app.route('/readiness', methods=['GET'])
+def call_readiness():
+    return 'OK', 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
